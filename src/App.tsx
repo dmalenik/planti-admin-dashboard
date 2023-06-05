67821c706a2b154b266b12924012b54628d3dac6
Dmitriy Malenik <dmalenik@gmail.com>
2023-06-05 15:02:36 +0300

    feat: create SideBar component and its styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.scss';
 import Body from './components/Body/Body';
+import SideBar from './components/SideBar/SideBar';
 
 const App = (): JSX.Element => {
   return (
     <div className='container'>
+      <SideBar />
       <Body />
     </div>
   );
---
+++ src/components/SideBar/SideBar.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './SideBar.scss';
+
+const SideBar = (): JSX.Element => {
+  return <div>SideBar</div>;
+};
+
+export default SideBar;
