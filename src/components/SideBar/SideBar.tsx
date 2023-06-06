5e41a4dbab3a34ae1a270712e1296b6e6f459ea0
Dmitriy Malenik <dmalenik@gmail.com>
2023-06-06 16:47:00 +0300

    feat: create SideBar component
    - create elements
    - set styles for elements

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -1,8 +1,87 @@
 import React from 'react';
 import './SideBar.scss';
 
+// Imported Images
+import logo from '../../assets/imgs/pexels-alan-cabello-1173217.jpg';
+
+// Imported Icons
+import { IoMdSpeedometer } from 'react-icons/io';
+import {
+  MdDeliveryDining,
+  MdOutlineExplore,
+  MdOutlinePermContactCalendar,
+} from 'react-icons/md';
+import { BsTrophy, BsCreditCard2Front } from 'react-icons/bs';
+import { AiOutlinePieChart } from 'react-icons/ai';
+import { BiTrendingUp } from 'react-icons/bi';
+
 const SideBar = (): JSX.Element => {
-  return <div>SideBar</div>;
+  return (
+    <div className='sideBar grid'>
+      <div className='logoDiv flex'>
+        <img src={logo} alt='Image Name' />
+        <h2>Planti.</h2>
+      </div>
+      <div className='menuDiv'>
+        <h3 className='divTitle'>QUICK MENU</h3>
+        <ul className='menuLists grid'>
+          <li className='listItem'>
+            <a href='' className='menuLink flex'>
+              <IoMdSpeedometer className='icon' />
+              <span className='smallText'>Dashboard</span>
+            </a>
+          </li>
+          <li className='listItem'>
+            <a href='' className='menuLink flex'>
+              <MdDeliveryDining className='icon' />
+              <span className='smallText'>My Orders</span>
+            </a>
+          </li>
+          <li className='listItem'>
+            <a href='' className='menuLink flex'>
+              <MdOutlineExplore className='icon' />
+              <span className='smallText'>Explore</span>
+            </a>
+          </li>
+          <li className='listItem'>
+            <a href='' className='menuLink flex'>
+              <BsTrophy className='icon' />
+              <span className='smallText'>Products</span>
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div className='settingsDiv'>
+        <h3 className='divTitle'>SETTINGS</h3>
+        <ul className='menuLists grid'>
+          <li className='listItem'>
+            <a href='' className='menuLink flex'>
+              <AiOutlinePieChart className='icon' />
+              <span className='smallText'>Charts</span>
+            </a>
+          </li>
+          <li className='listItem'>
+            <a href='' className='menuLink flex'>
+              <BiTrendingUp className='icon' />
+              <span className='smallText'>Trends</span>
+            </a>
+          </li>
+          <li className='listItem'>
+            <a href='' className='menuLink flex'>
+              <MdOutlinePermContactCalendar className='icon' />
+              <span className='smallText'>Contact</span>
+            </a>
+          </li>
+          <li className='listItem'>
+            <a href='' className='menuLink flex'>
+              <BsCreditCard2Front className='icon' />
+              <span className='smallText'>Billing</span>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default SideBar;
