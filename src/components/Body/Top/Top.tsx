edeb29c5953e1748798f274f3e07cf6acc3c41c8
Dmitriy Malenik <dmalenik@gmail.com>
2023-06-16 22:29:36 +0300

    feat: add structure for main part of the left card

--- src/components/Body/Top/Top.tsx
+++ src/components/Body/Top/Top.tsx
@@ -4,8 +4,10 @@ import './Top.scss';
 import { BiSearchAlt } from 'react-icons/bi';
 import { TbMessageCircle } from 'react-icons/tb';
 import { IoMdNotificationsOutline } from 'react-icons/io';
+import { BsArrowRightShort } from 'react-icons/bs';
 
 import adminImage from '../../../assets/imgs/2019-04-03_IMG_0457_ret_35x45 mm.jpg';
+import someImage from '../../../assets/imgs/10995888_911070795590725_8814513449878722782_n.jpg';
 
 import rainyVideo from '../../../assets/vds/pexels-maksim-romashkin-12717007-3840x2160-25fps.mp4';
 
@@ -41,6 +43,41 @@ const Top = (): JSX.Element => {
           </div>
           <div className='videoDiv'>
             <video src={rainyVideo} autoPlay loop muted></video>
+          </div>
+        </div>
+        <div className='leftCard flex'>
+          <div className='main flex'>
+            <div className='textDiv'>
+              <h1>My Stats</h1>
+              <div className='flex'>
+                <span>
+                  Today <br /> <small>4 Orders</small>
+                </span>
+                <span>
+                  This Month <br />
+                  <small>127 Orders</small>
+                </span>
+              </div>
+              <span className='flex link'>
+                Go to my orders <BsArrowRightShort className='icon' />
+              </span>
+            </div>
+            <div className='imageDiv'>
+              <img src={someImage} alt='Image Name' />
+            </div>
+            {/* <div className='sideBarCard'>
+              <BsQuestionCircle className='icon' />
+              <div className='cardContent'>
+                <div className='circle1'></div>
+                <div className='circle2'></div>
+                <h3>Help Center</h3>
+                <p>
+                  Having troubles in Planti, please contact us for more
+                  questions
+                </p>
+                <button className='btn'>Go to Help Center</button>
+              </div>
+            </div> */}
           </div>
         </div>
       </div>
