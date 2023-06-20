3acc00b495316056ccf1fa46091b25e2395d47ed
Dmitriy Malenik <dmalenik@gmail.com>
2023-06-20 11:27:07 +0300

    feat: create Top Sellers component
    - add assets
    - write a structure
    - write styles

--- src/components/Body/Listing/Listing.tsx
+++ src/components/Body/Listing/Listing.tsx
@@ -7,6 +7,9 @@ import { BsArrowRightShort } from 'react-icons/bs';
 import leavesImg from '../../../assets/imgs/pexels-felipe-alves-10616343.jpg';
 import palacePlantsImg from '../../../assets/imgs/pexels-maria-orlova-4946907.jpg';
 import mointainsViewImg from '../../../assets/imgs/pexels-tim-gouw-5677269.jpg';
+import plantsAndWaterfallImg from '../../../assets/imgs/pexels-antonio-friedemann-3890544.jpg';
+import flowersAndMountainsImg from '../../../assets/imgs/pexels-dominik-simecek-4807058.jpg';
+import someImg2 from '../../../assets/imgs/pexels-madison-inouye-1054024.jpg';
 
 const Listing = (): JSX.Element => {
   return (
@@ -32,6 +35,29 @@ const Listing = (): JSX.Element => {
           <AiFillHeart className='icon' />
           <img src={mointainsViewImg} alt='Image Name' />
           <h3>Mountains View</h3>
+        </div>
+      </div>
+      <div className='sellers flex'>
+        <div className='topSellers'>
+          <div className='heading flex'>
+            <h3>Top Sellers</h3>
+            <button className='btn flex'>
+              See All <BsArrowRightShort className='icon' />
+            </button>
+          </div>
+          <div className='card flex'>
+            <div className='users'>
+              <img src={plantsAndWaterfallImg} alt='Image Name' />
+              <img src={flowersAndMountainsImg} alt='Image Name' />
+              <img src={someImg2} alt='Image Name' />
+            </div>
+            <div className='cardText'>
+              <span>14.456 Plants sold</span> <br />
+              <small>
+                21 Sellers <span className='date'>7 Days</span>
+              </small>
+            </div>
+          </div>
         </div>
       </div>
     </div>
