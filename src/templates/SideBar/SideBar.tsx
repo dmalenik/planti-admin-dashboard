3823392cf40393b8726f037ec14877a336365fb8
Dmitriy Malenik <dmalenik@gmail.com>
2023-06-29 01:23:24 +0300

    feat: create templates folder

--- src/templates/SideBar/SideBar.tsx
+++ src/templates/SideBar/SideBar.tsx
@@ -1,2 +1,15 @@
+import logo from '../../assets/imgs/pexels-alan-cabello-1173217.jpg';
 import { Menu, HelpCenter } from './components/';
 import { quickMenuData, settingsMenuData, helpCenterData } from './data';
+import './SideBar.scss';
+
+const SideBar = (): JSX.Element => (
+  <div className='sideBar grid'>
+    <div className='logoDiv flex'>
+      <img src={logo} alt='Image Name' />
+      <h2>Planti.</h2>
+    </div>
+  </div>
+);
+
+export default SideBar;
