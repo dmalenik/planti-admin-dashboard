da47f550193a2681b70f7f2792cdcb6e1bc6fcff
Dmitriy Malenik <dmalenik@gmail.com>
2023-06-30 17:20:16 +0300

    feat(Listing): create assets folder

---
+++ src/components/Body/components/Listing/data/cards.tsx
@@ -0,0 +1,4 @@
+import { AiFillHeart, AiOutlineHeart } from 'react-icons/ai';
+import leavesImg from '../assets/pexels-felipe-alves-10616343.jpg';
+import palacePlantsImg from '../assets/pexels-maria-orlova-4946907.jpg';
+import mointainsViewImg from '../assets/pexels-tim-gouw-5677269.jpg';
---
+++ src/components/Body/components/Listing/data/topSellers.tsx
@@ -0,0 +1,4 @@
+import { BsArrowRightShort } from 'react-icons/bs';
+import plantsAndWaterfallImg from '../assets/pexels-antonio-friedemann-3890544.jpg';
+import flowersAndMountainsImg from '../assets/pexels-dominik-simecek-4807058.jpg';
+import someImg2 from '../assets/pexels-madison-inouye-1054024.jpg';
