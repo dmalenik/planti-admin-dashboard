bdc061ad42b403fd5c5ba0089af2c4cd85bb1445
Dmitriy Malenik <dmalenik@gmail.com>
2023-07-07 15:20:41 +0300

    feat: remove warning for constant exports

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,9 +9,6 @@ module.exports = {
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    'react-refresh/only-export-components': ['warn'],
   },
 };
