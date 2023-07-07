e2358e3a9271a667250b59e3d58ad0bacd807e88
Dmitriy Malenik <dmalenik@gmail.com>
2023-07-07 22:19:06 +0300

    feat: change base param

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://dmalenik.github.io/planti-admin-dashboard/',
+  base: 'dist',
   plugins: [react()],
   optimizeDeps: {
     force: true,
