c20a1362d96c1dae0074c97e2734f30837946cd6
Dmitriy Malenik <dmalenik@gmail.com>
2023-07-07 22:54:47 +0300

    feat: generate bundles

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://dmalenik.github.io/planti-admin-dashboard/',
   plugins: [react()],
   optimizeDeps: {
     force: true,
