e726bf393717e3f0ad95f122e79e6fefff97abf7
Dmitriy Malenik <dmalenik@gmail.com>
2023-07-07 15:22:00 +0300

    feat: edit vite options
    - set forced reload while changes are applied
    - exclude react-icons dependency from dependencies optimization
    - create manual chunks for builds

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  optimizeDeps: {
+    force: true,
+    exclude: ['react-icons'],
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          reactIcons: ['react-icons'],
+          videos: ['./src/shared/assets/videos'],
+        },
+      },
+    },
+  },
+});
