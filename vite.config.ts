512b5bb43f8429196e400c29a6818a5e0c23ae20
Dmitriy Malenik <dmalenik@gmail.com>
2023-07-09 09:21:53 +0300

    feat: change base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://dmalenik.github.io/',
+  base: './',
   plugins: [react()],
   optimizeDeps: {
     force: true,
