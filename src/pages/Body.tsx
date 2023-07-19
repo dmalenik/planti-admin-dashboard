72fae84823cb6e2de8537ae0918f5361c8e0fce1
Dmitriy Malenik <dmalenik@gmail.com>
2023-07-19 08:32:18 +0300

    refactor(Body): place styles imports under data imports

--- src/pages/Body.tsx
+++ src/pages/Body.tsx
@@ -1,9 +1,9 @@
-import './Body.scss';
 /* 
 ** do not import templates with await import
 ** causes blank page on production
 */
 import { Top, Listing, Activity } from '../templates';
+import './Body.scss';
 
 const Body = () => {
   return (
